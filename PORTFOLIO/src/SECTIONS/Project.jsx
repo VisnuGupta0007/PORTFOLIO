104bf6f8862b41fbebe6e1694acb90ce6970071d
Visnu Gupta <vg8383654@gmail.com>
2025-05-22 02:10:57 +0530

    Update Project.jsx

--- PORTFOLIO/src/SECTIONS/Project.jsx
+++ PORTFOLIO/src/SECTIONS/Project.jsx
@@ -17,7 +17,7 @@ function Project() {
           <div className="flex justify-center">
             <img
               src="https://tse1.mm.bing.net/th?id=OIP.Or4sEmf4dlDEcO_NpYL8-gHaFr&pid=Api&P=0&h=180"
-              alt="Card 1"
+              alt="Frontend_Landing"
               className="w-full h-60 md:h-80 border-2 rounded-3xl shadow-2xl object-cover"
             />
           </div>
@@ -38,7 +38,7 @@ function Project() {
           <div className="flex justify-center">
             <img
               src="https://static-00.iconduck.com/assets.00/uber-icon-1024x1024-4icncyyo.png"
-              alt="Card 4"
+              alt="Clone_Uber"
               className="w-full h-60 md:h-80 border-2 rounded-3xl shadow-2xl object-cover"
             />
           </div>
@@ -80,7 +80,7 @@ function Project() {
           <div className="flex justify-center">
             <img
               src="https://i.pinimg.com/736x/d9/8f/dd/d98fdd5c30de63fd1c79cbc380eb43c7.jpg"
-              alt="Card 2"
+              alt="Clone_KFC"
               className="w-full h-60 md:h-80 border-2 rounded-3xl shadow-2xl object-cover"
             />
           </div>
@@ -97,13 +97,33 @@ function Project() {
         </div>
 
 
-
         {/* Card 5 */}
         <div className="border-2 p-6 md:p-10 rounded-3xl shadow-2xl w-full max-w-md">
           <div className="flex justify-center">
             <img
+              src="https://user-images.githubusercontent.com/39626451/192898250-711e2281-ab03-433a-afeb-4ad542b68a5b.png"
+              alt="BootStrap_Landing"
+              className="w-full h-60 md:h-80 border-2 rounded-3xl shadow-2xl object-cover"
+            />
+          </div>
+          <button className="flex shadow-xl items-center justify-center h-12 p-4 border-2 rounded-full mt-5 hover:bg-gray-800 hover:text-white hover:animate-pulse w-full">
+            <a
+              href="https://bootstrap-landing-five.vercel.app/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="font-mono"
+            >
+              Go Live
+            </a>
+          </button>
+        </div>
+
+        {/* Card 6 */}
+        <div className="border-2 p-6 md:p-10 rounded-3xl shadow-2xl w-full max-w-md">
+          <div className="flex justify-center">
+            <img
               src="https://tse1.mm.bing.net/th?id=OIP.iGZO46DdQlFctZ-03Hq2HgHaEm&pid=Api&P=0&h=180"
-              alt="Card 4"
+              alt="Amazon"
               className="w-full h-60 md:h-80 border-2 rounded-3xl shadow-2xl object-cover"
             />
           </div>
